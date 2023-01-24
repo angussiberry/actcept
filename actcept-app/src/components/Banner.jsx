07cb0807c63f85a56943f33749d6442fc4c6e088
AsfandyarTassadaq <asfandyar.tassadaq@accenture.com>
2023-01-24 12:38:38 +0000

    Added a

--- actcept-app/src/components/Banner.jsx
+++ actcept-app/src/components/Banner.jsx
@@ -2,14 +2,18 @@ import '../App.css';
 
 function Banner() {
   return (
-    <div className="Banner">
-      <div>
-        <h1 className="banner-header">ActCept</h1>
-        <h4 className="banner-subtitle">Part of GigStr</h4>
+    <div className="Banner" style={{ backgroundImage: 'url("https://cdn.pixabay.com/photo/2015/11/22/19/04/crowd-1056764_960_720.jpg")' }} >
+      <div className='banner-overlay'>
+        <div className='banner-event'>
+          <h1 className="banner-header">props.event_name</h1>
+          <h4 className="banner-subtitle">props.event_desc
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi qui, obcaecati, nostrum unde, doloribus ipsam consectetur nulla officia facere fugit vero repellendus! Vel quas recusandae unde reiciendis dolorem culpa accusamus?</h4>
+        </div>
+        <button className='btn banner-btn'>See more</button>
       </div>
-      <h4 className="banner-desc">Find the right event for you!</h4>
+
     </div>
   );
 }
-
+//
 export default Banner;
