b0fbc19782f7aa2fd4ba916bda25da9f764d60f8
AsfandyarTassadaq <asfandyar.tassadaq@accenture.com>
2023-01-25 09:30:14 +0000

    Made date human readable

--- actcept-app/src/components/Content.jsx
+++ actcept-app/src/components/Content.jsx
@@ -18,44 +18,7 @@ function Content() {
     fetchData();
   }, []);
 
-  const cards =
-    <>
-      <Card
-        title="Hello"
-        img="https://cdn.pixabay.com/photo/2015/05/31/15/07/coffee-792113_960_720.jpg"
-        desc="This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
-        date="10/12/2023" />
-      <Card
-        title="this"
-        img="https://cdn.pixabay.com/photo/2017/01/04/15/59/feather-1952382_960_720.jpg"
-        desc="This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
-        date="10/12/2023" />
-      <Card
-        title="is"
-        img="https://cdn.pixabay.com/photo/2015/05/28/14/53/ux-788002_960_720.jpg"
-        desc="This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
-        date="10/12/2023" />
-      <Card
-        title="is"
-        img="https://cdn.pixabay.com/photo/2015/05/28/14/53/ux-788002_960_720.jpg"
-        desc="This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
-        date="10/12/2023" />
-      <Card
-        title="is"
-        img="https://cdn.pixabay.com/photo/2015/05/28/14/53/ux-788002_960_720.jpg"
-        desc="This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
-        date="10/12/2023" />
-      <Card
-        title="a"
-        img="https://cdn.pixabay.com/photo/2018/01/03/05/33/the-sun-3057622_960_720.jpg"
-        desc="This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
-        date="10/12/2023" />
-      <Card
-        title="CARD"
-        img="https://cdn.pixabay.com/photo/2019/05/10/20/11/roof-truss-4194408_960_720.jpg"
-        desc="This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
-        date="10/12/2023" />
-    </>
+
   return (
     <div>
       <Search />
@@ -65,10 +28,9 @@ function Content() {
             title={event.event_name}
             img={event.image_url}
             desc={event.event_description}
-            date={event.event_date}
+            date={event.event_date.split('T')[0]}
           />
         ))}
-        {cards}
 
       </div>
     </div>
