c8285cb86491d14b224c102d97cee7dc543115bd
AsfandyarTassadaq <asfandyar.tassadaq@accenture.com>
2023-01-25 12:52:04 +0000

    Added link, need to fix css

--- actcept-app/src/pages/EventDescription.jsx
+++ actcept-app/src/pages/EventDescription.jsx
@@ -1,23 +1,42 @@
 import React from 'react';
 import EventDescriptionBanner from '../components/EventDescriptionBanner';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import Header from '../components/Header';
 import EventInfo from '../components/EventInfo';
 import EventContent from '../components/EventContent';
+import { useState, useEffect } from 'react';
 // import GoogleMap from '../components/GoogleMap';
 
 
 function EventDescription() {
+    const location = useLocation();
+    const state = location.state;
+    console.log(state)
+    const [event, setEvent] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`https://4o3xjugkz1.execute-api.eu-west-2.amazonaws.com/dev/events?event_id=${state}`);
+                const data = await response.json();
+                setEvent(data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchData();
+    }, []);
     return (
         <div className="EventDescription">
             <Header />
             <div className="main-content">
-                <EventDescriptionBanner
-                    event_name = "Event Name"/>
+                {event.length ? <EventDescriptionBanner
+                    event_name={event[0].event_name}
+                    img={event[0].image_url} /> : null}
+
                 <div className="event-info">
-                    <EventInfo/>
-                    <br/>
-                    <EventContent/>              
+                    <EventInfo />
+                    <br />
+                    <EventContent />
                 </div>
             </div>
         </div>
